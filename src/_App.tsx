e0967fb858814f27346ecc6f7a21552f2dd9b4fc
Paulo Jalowyj <paulo.jalowyj@outlook.com>
2021-03-22 11:01:17 -0300

    🎉 Desafio Finalizado

--- src/_App.tsx
+++ src/_App.tsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from 'react';
 
-import { SideBar } from './components/SideBar';
-import { Content } from './components/Content';
+import { Button } from './components/Button';
+import { MovieCard } from './components/MovieCard';
+
+// import { SideBar } from './components/SideBar';
+// import { Content } from './components/Content';
 
 import { api } from './services/api';
 
 import './styles/global.scss';
+
+import './styles/sidebar.scss';
+import './styles/content.scss';
 
 interface GenreResponseProps {
   id: number;
@@ -14,8 +20,8 @@ interface GenreResponseProps {
 }
 
 interface MovieProps {
-  Title: string;
   imdbID: string;
+  Title: string;
   Poster: string;
   Ratings: Array<{
     Source: string;
@@ -54,16 +60,36 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <SideBar 
-        genres={genres}
-        handleClickButton={handleClickButton}
-        selectedGenreId={selectedGenreId}
-      />
+      <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
 
-      <Content 
-        movies={movies}
-        selectedGenre={selectedGenre}
-      />
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+
+      <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
     </div>
   )
 }
